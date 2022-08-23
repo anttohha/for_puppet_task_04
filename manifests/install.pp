38fd521920c2e8e010683036bd315cfd17cf5f42
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-23 20:51:25 +0300

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 node 'slave1.puppet' {
               service {'httpd':
-    ensure  => running,
-    require => Package['httpd'],
+              ensure  => running,
+              require => Package['httpd'],
   }
 }
