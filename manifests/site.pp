dd2fde6c09a09d907681f092bff444c5495f1efe
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-23 22:48:02 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,16 +18,7 @@ class php {
 
 node 'slave1.puppet'{
   include httpd
-  
-  -> file { '/var/www/html/':   
-        ensure  => directory,   
-        source  => 'puppet:///file/'index.html',  
-        recurse => true,   
-        purge   => true,  
-        force   => true, 
-    }
-  
-  
+    
 }
 
 node 'slave2.puppet'{
