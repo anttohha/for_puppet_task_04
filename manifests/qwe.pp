7e17d9a72683198770ab2252afbba95669f3f661
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-23 22:49:08 +0300

    Update and rename qwe to qwe.pp

--- manifests/qwe.pp
+++ manifests/qwe.pp
@@ -1,7 +1,9 @@
--> file { '/var/www/html/':   
+node 'slave1.puppet'{
+ -> file { '/var/www/html/':   
         ensure  => directory,   
         source  => 'puppet:///file/'index.html',  
         recurse => true,   
         purge   => true,  
         force   => true, 
     }
+}
