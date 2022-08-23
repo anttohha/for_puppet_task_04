25ebc101484dc5f5be9da27005c3d1a5d338991e
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-23 22:50:37 +0300

    Delete qwe.pp

--- manifests/qwe.pp
+++
@@ -1,9 +0,0 @@
-node 'slave1.puppet'{
-    file { '/var/www/html/':   
-        ensure  => directory,   
-        source  => 'puppet:///file/'index.html',  
-        recurse => true,   
-        purge   => true,  
-        force   => true, 
-    }
-}
