ae01f6a568b20797540fc1f52232a47c59812221
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-23 20:18:11 +0300

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,16 +3,3 @@ node 'slave1.puppet' {
               ensure => installed,
                        }
 }
-
-node 'slave2.puppet' {
-              package { 'php' :
-              ensure => installed,
-                       }
-}
-
-node 'slave2.puppet' {
-              package { 'httpd' :
-              ensure => installed,
-                       }
-}
-
