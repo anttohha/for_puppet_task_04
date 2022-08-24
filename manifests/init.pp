d6ba51ef4f91af55151a4312d806f72a402723a8
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-24 09:34:00 +0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,5 @@
-  class mymodule {file { '/var/www/html':
-        ensure => directory,
-        owner => 'root',
-        group => 'root',
-        
-    }file { "/var/www/html/index.html":
+  class mymodule {
+       file { "/var/www/html/index.html":
         mode => "0644",
-        owner => 'root',
-        group => 'root',
         source => 'puppet:///modules/files/index.html',
     }
