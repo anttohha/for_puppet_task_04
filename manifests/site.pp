f85abab8b6f44b0a4278a73348a102a2423a5d4f
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-24 10:52:34 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,3 @@
-class htmlka {
-  package {['httpd']:
-    ensure => installed
-  }
-  
-  file { '/var/www/html':
-    ensure => directory
-  }
-  
-  file { '/var/www/html/index.html':
-    ensure => file,
-    source => 'puppet:///modules/files/index.html'
-  }
-  
- service {'httpd':
-    ensure => running
- }
-}
-
 node 'slave1.puppet'{
-  include htmlka
-  
-  
-    
+  include htmlka      
 }
