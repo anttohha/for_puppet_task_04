6d285fdae72a4d561b8fef9d741114055f26a9e8
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-29 10:13:40 +0300

    Update init.pp

--- modules/maincraft/manifests/init.pp
+++ modules/maincraft/manifests/init.pp
@@ -1,15 +1,8 @@
 class maincraft {
  
  file { '/opt/minecraft/':
-    ensure => directory
-    
- include ::wget
-
-     wget::fetch { 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar':
-    destination => '/opt/minecraft/server.jar',    
-  }
-    
-    
+    ensure => directory 
+ 
   }
  
  }
