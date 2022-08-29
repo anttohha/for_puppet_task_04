779507c8ab5929904ad022b8b4bfff712057bc5a
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-29 09:16:42 +0300

    Create init.pp

---
+++ modules/maincraft/manifests/init.pp
@@ -0,0 +1,9 @@
+exec{'retrieve_leiningen':
+  command => "/usr/bin/wget -q https://raw.github.com/technomancy/leiningen/stable/bin/lein -O /home/vagrant/bin/lein",
+  creates => "/home/vagrant/bin/lein",
+}
+
+file{'/home/vagrant/bin/lein':
+  mode => 0755,
+  require => Exec["retrieve_leiningen"],
+}
