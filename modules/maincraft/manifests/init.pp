b1c4818ddc010f119795431840136c4729ccf01a
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-29 10:10:43 +0300

    Update init.pp

--- modules/maincraft/manifests/init.pp
+++ modules/maincraft/manifests/init.pp
@@ -4,12 +4,16 @@ class maincraft {
     ensure => directory
   }
  
- exec{'retrieve_leiningen':
-   command => "/usr/bin/wget https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar -O /opt/minecraft",   
  }
 
 
- file{'/opt/minecraft/server.jar':
-   require => Exec["retrieve_leiningen"],
- }
+class fetch_file {
+
+  include ::wget
+
+  wget::fetch { 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar':
+    destination => '/opt/minecraft/server.jar',
+    verbose     => true,
+  }
+
 }
