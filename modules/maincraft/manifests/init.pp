c0e0c99765ab96140c7d8e561188b7039ce38e76
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-29 10:42:02 +0300

    Update init.pp

--- modules/maincraft/manifests/init.pp
+++ modules/maincraft/manifests/init.pp
@@ -8,6 +8,13 @@ class maincraft {
  
   }
   
+  file { "/opt/minecraft/newjava.rpm":
+  source => [
+    "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246798_424b9da4b48848379167015dcc250d8d",
+  ]
+}
+  
+  
   file { "/opt/minecraft/server.jar":
   source => [
     "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar",
