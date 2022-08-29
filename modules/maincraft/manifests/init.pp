2b7b14c33d43295e222dffa93766ad05761a043d
anttohha <100775019+anttohha@users.noreply.github.com>
2022-08-29 10:33:07 +0300

    Update init.pp

--- modules/maincraft/manifests/init.pp
+++ modules/maincraft/manifests/init.pp
@@ -5,11 +5,11 @@ class maincraft {
  
   }
   
-  file { '/opt/minecraft/server.jar':
-    source => httpget('https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar'),
-                
-}  
-  
+  file { "/opt/minecraft/server.jar":
+  source => [
+    "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar",
+  ]
+}
  
  }
 
